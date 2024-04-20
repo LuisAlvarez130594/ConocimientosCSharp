15700cb0032bc19ac554586f8e509c9207395197
Luis Alvarez <juan.alvarez@elipgo.com>
2024-04-19 18:13:16 -0600

    Ejercicio Asignado examen practico

---
+++ ConocimientosC#/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Days = new List<string> { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
+            //string[] Days = { "Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" };
+            Console.Write("Ingresa un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            string NombreDia = Days[numero - 1];
+            Console.WriteLine("Tu dia es: " + NombreDia);
+            Console.WriteLine("Presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
